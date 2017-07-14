d4d22d5441cb4ebab2dea9ae3ac0ac655cd60d0f
Nilaksh Bajpai <nilaksh.bajpai@gmail.com>
2017-07-15 00:20:31 +0530

    Update QwertyJaroWinklerDistance.java
    Added GNU GPLv3 license

--- src/main/java/com/myntra/charles/QwertyJaroWinklerDistance.java
+++ src/main/java/com/myntra/charles/QwertyJaroWinklerDistance.java
@@ -7,10 +7,20 @@ import org.apache.lucene.search.spell.StringDistance;
 /**
  * QWERTY distance implementation of JaroWinkler distance.
  * 
- * @author 11169
+ * Copyright (C) 2017 Nilaksh Bajpai
  * 
- * @see <a href="http://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance">
- *      http://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance</a>
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 public class QwertyJaroWinklerDistance implements StringDistance {
 
